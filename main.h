36fc942ff717c24640a9a116d1469c86a19e93fe
AndroGenius-codes <70571088+AndroGenius-codes@users.noreply.github.com>
2022-10-18 20:55:08 +0100

    Create main.h

---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * struct print - struct for printer functions
+ * @type_arg: identifier
+ * @f: pointer to a printer functions
+ *
+ * Description: struct that stores pointers to a
+ * printer functions.
+ */
+typedef struct print
+{
+	char *type_arg;
+	int (*f)(va_list, char *, unsigned int);
+} print_t;
+
+int _printf(const char *format, ...);
+int print_int(va_list arguments, char *buf, unsigned int ibuf);
+unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
+int print_buf(char *buf, unsigned int nbuf);
